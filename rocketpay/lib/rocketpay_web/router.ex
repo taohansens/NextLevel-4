5805ac32978640ef4dd89d54bf405a6c1d930ad8
Tao Hansen <taohansens@gmail.com>
2021-02-24 20:36:49 -0300

    created UsersController, UsersView. Add Route

--- rocketpay/lib/rocketpay_web/router.ex
+++ rocketpay/lib/rocketpay_web/router.ex
@@ -9,6 +9,8 @@ defmodule RocketpayWeb.Router do
     pipe_through :api
 
     get "/:filename", WelcomeController, :index
+
+    post "/users", UsersController, :index
   end
 
   # Enables LiveDashboard only for development
